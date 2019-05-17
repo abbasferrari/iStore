fd0ec335ccf55201e61ac91121fd2c2ce7ae5eea
Abbas Mohamed <mabbasm@vmware.com>
2019-05-17 12:30:03 +0530

    running the prod code

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/istore'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/src/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/istore/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
