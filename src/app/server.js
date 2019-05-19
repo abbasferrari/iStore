13355bdb481d068d80e6adb830e0073a5aae0321
Abbas Mohamed <mabbasm@vmware.com>
2019-05-19 21:02:54 +0530

    gonna complete it now

--- src/app/server.js
+++ src/app/server.js
@@ -1,16 +1,23 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', function(req,res) {
+var corsOptions = {
+    origin: 'https://lit-eyrie-15561.herokuapp.com/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/iStore'));
+
+app.get('/*', cors(corsOptions),function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/iStore/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
